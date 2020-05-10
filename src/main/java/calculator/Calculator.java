d0d576f57328d0f7546e53a03b9e91256e754057
SaiSriHarsha333 <Sai.SriHarsha@iiitb.org>
2020-05-10 20:08:11 +0530

    Changes to Calculator.java

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -41,7 +41,7 @@ public static double divide(double num1, double num2){
             System.out.println("1) Addition");
             System.out.println("2) Subtraction");
             System.out.println("3) Multiplication");
-            // System.out.println("4) Division");
+            System.out.println("4) Division");
             System.out.println("5) Exit");
             System.out.print("Enter your choice: ");
             ch = reader.nextInt();
